3450261f7d18c9bb507330c60b7846969722d2ef
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-04-01 14:22:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ import { loadMetar } from "./js/metar.js";
 import { loadTaf } from "./js/taf.js";
 import { loadFids } from "./js/fids.js";
 import { initHeatmapToggle } from "./js/ui.js";
+import { initHeatmapToggle, initHeatmapHistory, initClusterToggle, initDebugPanel } from "./js/ui.js";
 
 window.onload = () => {
     window.map = initMap();
     initUI();
     initHeatmapToggle(window.map);
+    initHeatmapHistory(window.map);
+    initClusterToggle(window.map);
+    initDebugPanel();
     loadMetar();
     loadTaf();
     loadFids();
-};
